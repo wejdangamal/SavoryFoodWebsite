b27cdb66a62d9a4f339a2b3a79cfbf243c84ce44
Wegdan Gamal <73383355+wejdangamal@users.noreply.github.com>
2023-12-14 00:36:13 +0200

    Delete login_img directory

--- login_img/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace login_img.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
